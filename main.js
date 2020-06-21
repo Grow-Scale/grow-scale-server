bc3bb79c0c99df12e0d598eb467f44fd3ac1e47e
yvkrishna <vedhakrishnapanchami@gmail.com>
2020-06-21 15:55:20 +0530

    wit has been  added

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 
 var mongojs = require("mongojs");
-var db = mongojs("mongodb://vedha:krishna123@cluster0-shard-00-00-kbuhh.mongodb.net:27017/Hutlabs?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",["members,project_wait_list,student_wait_list,ongoing_projects"]);
+var db = mongojs("mongodb://vedha:krishna123@cluster0-shard-00-00-kbuhh.mongodb.net:27017/FBHackathon?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",["problems"]);
 var express=require("express");
 var bodyParser=require('body-parser');
 var app = express();
+const {Wit, log} = require('node-wit');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
+const client = new Wit({
+	accessToken: 'AS55FM2MNRFKM4TEF7WR3MYCPFPMQX2J',
+	logger: new log.Logger(log.DEBUG) // optional
+  });
 app.get("/",function(req,res)
 {
     // Use this route for the front end tasks.
@@ -16,8 +21,11 @@ app.get("/",function(req,res)
 
 app.get("/fbhackathon",function(req,res)
 {
-    // Use this route for the Back end tasks.
-	res.send("Use This Route For Back End Works");
+	client.message("The clothes are torn out and the quality was too bad.",{})
+	.then((data)=>{
+		console.log(data);
+		res.send(Object.values(data.entities));
+	})
 });
 
 app.listen(4000,function(){
